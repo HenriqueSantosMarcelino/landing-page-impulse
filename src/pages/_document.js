6d74011ec16248b4e7de709b92d063bc759d996e
HenriqueSantosMarcelino <sm26.henrique@gmail.com>
2025-01-22 00:14:46 -0300

    Declaração de Meta Tags básicas e ajustes na arquitetura

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,12 +2,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+<Html lang="pt-br">
+      <Head>
+        <meta charSet="UTF-8" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
